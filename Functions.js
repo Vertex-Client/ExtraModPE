10b33acf55d2a6edeb47892031582615e0094644
Vertex Client <Vertex-Client@users.noreply.github.com>
2017-04-15 23:25:06 +0200

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -1,18 +1,26 @@
 Player.getAmountOfItemInInv = function(id, data) {
-	let count = 0;
-	for(let i = 9; i < 45; i++) {
-		if(Player.getInventorySlot(i) == id && Player.getInventorySlotData(i) == data) {
-			count += Player.getInventorySlotCount(i);
+	try {
+		let count = 0;
+		for(let i = 9; i < 45; i++) {
+			if(Player.getInventorySlot(i) == id && Player.getInventorySlotData(i) == data) {
+				count += Player.getInventorySlotCount(i);
+			}
 		}
+		return count;
+	} catch(e) {
+		clientMessage("Error: " + e);
 	}
-	return count;
 }
 
 Player.addOrDropItem = function(id, amount, data) {
-    let count_before = Player.getAmountOfItemInInv(id, data);
-    Player.addItemInventory(id, amount, data);
-    let count_after = Player.getAmountOfItemInInv(id, data);
-    if(!(count_after > count_before)) {
-        Level.dropItem(getPlayerX(), getPlayerY(), getPlayerZ(), 0, id, amount, data);
-    }
+	try {
+		let count_before = Player.getAmountOfItemInInv(id, data);
+		Player.addItemInventory(id, amount, data);
+		let count_after = Player.getAmountOfItemInInv(id, data);
+		if(!(count_after > count_before)) {
+			Level.dropItem(getPlayerX(), getPlayerY(), getPlayerZ(), 0, id, amount, data);
+		}
+	} catch(e) {
+		clientMessage("Error: " + e);
+	}
 }
